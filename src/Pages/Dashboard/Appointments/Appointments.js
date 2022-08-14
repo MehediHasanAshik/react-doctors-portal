f2b7e877766f6e023ba0c573a84379ab1d7d8523
Mehedi Hasan Ashik <mhashik.ashik@gmail.com>
2022-08-15 00:00:01 +0600

    datefixed

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -8,7 +8,7 @@ const Appointments = ({ date }) => {
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        const url = `https://intense-sands-21625.herokuapp.com/appointments?email=${user.email}&date=${date}`
+        const url = `https://intense-sands-21625.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url)
             .then(res => res.json())
             .then(data => setAppointments(data))
